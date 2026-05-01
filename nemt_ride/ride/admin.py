ec8203c0ccd38b96d06e8ea9af4a47f65f09097f
javierd23 <carlosjjavier05@gmail.com>
2026-04-30 22:06:02 -0400

    Adding admin

--- nemt_ride/ride/admin.py
+++ nemt_ride/ride/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ride, RideStatus, Ride_Event
+
+
+@admin.register(RideStatus)
+class RideStatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status')
+    search_fields = ('status',)
+
+
+class RideEventInline(admin.TabularInline):
+    model = Ride_Event
+    extra = 0
+    readonly_fields = ('created_at',)
+    fields = ('description', 'created_at')
+
+
+@admin.register(Ride)
+class RideAdmin(admin.ModelAdmin):
+    list_display = ('id_ride', 'status', 'id_rider', 'id_driver', 'pickup_time', 'updated_at')
+    list_filter = ('status',)
+    search_fields = ('id_rider__email', 'id_driver__email')
+    readonly_fields = ('updated_at',)
+    autocomplete_fields = ('id_rider', 'id_driver')
+    inlines = (RideEventInline,)
+
+    fieldsets = (
+        (None, {'fields': ('status', 'id_rider', 'id_driver', 'pickup_time')}),
+        ('Pickup location', {'fields': ('pickup_latitude', 'pickup_longitude')}),
+        ('Dropoff location', {'fields': ('dropoff_latitude', 'dropoff_longitude')}),
+        ('Timestamps', {'fields': ('updated_at',)}),
+    )
+
+
+@admin.register(Ride_Event)
+class RideEventAdmin(admin.ModelAdmin):
+    list_display = ('id_ride_event', 'id_ride', 'description', 'created_at')
+    search_fields = ('id_ride__id_ride', 'description')
+    readonly_fields = ('created_at',)
--- nemt_ride/user_auth/admin.py
+++ nemt_ride/user_auth/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User, UserRoles
+
+
+@admin.register(UserRoles)
+class UserRolesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'role')
+    search_fields = ('role',)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ('email',)
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('role', 'is_active', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ('date_joined', 'last_login')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Role & permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('date_joined', 'last_login')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'phone_number', 'role'),
+        }),
+    )
